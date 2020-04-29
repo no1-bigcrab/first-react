d79922ab1c1540c4b506c01336a95605e1219aa2
no1-bigcrab <nhiepphongts@gmail.com>
2020-04-29 23:13:36 +0700

    nhúng jquery bootsatrap

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+        <h2>
+            Hello đây là header!!
+        </h2>
+    </div>
+  );
+}
+
+export default Header;
