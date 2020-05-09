3d8b3cfffd1e522f48a7894141b7589a9378e0e9
no1-bigcrab <nhiepphongts@gmail.com>
2020-05-09 14:23:35 +0700

    props với react

--- src/App.js
+++ src/App.js
@@ -1,32 +1,84 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-import { Home } from './Home';
-import { About } from './About';
-import { Contact } from './Contact';
-import { NoMatch } from './NoMatch';
-import { Layout } from './components/Layout';
-import { NavigationBar } from './components/NavigationBar';
+import ProductComponent from './components/ProductComponent';
 
 import './App.css';
 
 class App extends Component {
   render() {
+    var products =[
+      {
+          id : 1,
+          name: "Apple 1234 cua",
+          price: 500,
+          image: "https://cdn.tgdd.vn/Products/Images/42/71306/iphone-6s-5-300x300.jpg",
+          status: true
+      },
+      {
+          id : 2,
+          name: "Iphone 8 plus 64Gb",
+          price: 700,
+          image: "https://i.ndh.vn/2019/09/11/4-1568186945.jpg",
+          status: false
+      },
+      {
+          id : 3,
+          name: "SamSung 6plus 64Gb",
+          price: 1100,
+          image: "https://www.techrum.vn/chevereto/images/2017/09/13/swfu2.jpg",
+          status: true
+      }
+    ];
+    let elements = products.map((product, index) => {
+      let result;
+      if (product.status) {
+          result = <ProductComponent
+                      key={product.id}
+                      price={ product.price }
+                      image={ product.image }
+                    >
+                      {product.name}
+                    </ProductComponent>
+      }
+        return  result;
+    });
+
     return (
-      <Layout>
-        <React.Fragment>
-            <NavigationBar/>
-            <Router>
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route  path="/about" component={About} />
-                <Route  path="/contact" component={Contact} />
-                <Route component={NoMatch} />
-              </Switch>
-            </Router>
-        </React.Fragment>
-      </Layout>
-        
+      <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" >Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" >Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" >Features</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" >Pricing</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled"  tabIndex={-1} aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="container">
+          <div className="row">
+              <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                  {elements}
+              </div>
+              <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                  <button type="button" className="btn btn-warning"> Click Me!</button>
+              </div>
+          </div>
+      </div>
+     
+
+      </>
     );
   } 
 }
---
+++ src/components/ProductComponent.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+
+class ProductComponent extends Component {
+
+    // constructor(props){
+    //     super(props);
+    //     this.onAddToCard = this.onAddToCard.bind(this);
+    // }
+
+    // onAddToCard(text){
+    //     console.log(text);
+    //     alert(this.props.children + '-' +this.props.price + 'VND');
+    // }
+    onAddToCard2 = () => {
+            //console.log(text);
+            alert(this.props.children + '-' +this.props.price + 'VND');
+        }
+
+    render () {
+
+        return (
+            <div>
+                <div className="col-lg-4 col-sm-4 col-md-4 col-lg-4 border mt-3 p-1">
+                    <div className="thumbnail text-center">
+                        <img src={this.props.image} alt="" height="300" width="300"/>
+                        <div className="caption">
+                            <h3>{this.props.children}</h3>
+                            <p> <span>Giá : </span> {this.props.price}$</p>
+                            <p>
+                                <a className="btn btn-primary" onClick={this.onAddToCard} >Mua ngay</a>
+                                <a className="btn btn-info"  onClick={this.onAddToCard2} >Xem chi tiết</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ProductComponent;
